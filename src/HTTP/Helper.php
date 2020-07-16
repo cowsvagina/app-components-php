3b5c49c11e4a26add67fee40bfedb654ffdcf428
hsldymq <hsldymq@qq.com>
2020-07-16 19:14:01 +0800

    add HTTP Helper

---
+++ src/HTTP/Helper.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NB\HTTP;
+
+class Helper
+{
+    /**
+     * 抄自GuzzleHTTP parse_query.
+     *
+     * 调整了空值策略,用空字符串取代null
+     *
+     * @param $str
+     * @param bool $urlEncoding
+     * @return array
+     */
+    public static function parseQuery($str, $urlEncoding = true)
+    {
+        $result = [];
+
+        if ($str === '') {
+            return $result;
+        }
+
+        if ($urlEncoding === true) {
+            $decoder = function ($value) {
+                return rawurldecode(str_replace('+', ' ', $value));
+            };
+        } elseif ($urlEncoding === PHP_QUERY_RFC3986) {
+            $decoder = 'rawurldecode';
+        } elseif ($urlEncoding === PHP_QUERY_RFC1738) {
+            $decoder = 'urldecode';
+        } else {
+            $decoder = function ($str) { return $str; };
+        }
+
+        foreach (explode('&', $str) as $kvp) {
+            $parts = explode('=', $kvp, 2);
+            $key = $decoder($parts[0]);
+            $value = isset($parts[1]) ? $decoder($parts[1]) : '';
+            if (!isset($result[$key])) {
+                $result[$key] = $value;
+            } else {
+                if (!is_array($result[$key])) {
+                    $result[$key] = [$result[$key]];
+                }
+                $result[$key][] = $value;
+            }
+        }
+
+        return $result;
+    }
+}
