1116c444911b3a7a00a921798942f535e673d9e8
hsldymq <hsldymq@qq.com>
2020-07-20 23:34:11 +0800

    update client

--- src/HTTP/Client/Client.php
+++ src/HTTP/Client/Client.php
@@ -4,7 +4,51 @@ declare(strict_types=1);
 
 namespace NB\HTTP\Client\Logging;
 
-class Client
-{
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
+class Client extends \GuzzleHttp\Client
+{
+    private Logger $logger;
+
+    public function __construct(array $config = [], Logger $logger = null)
+    {
+        parent::__construct($config);
+        $this->logger = $logger;
+
+        $config = $this->getConfig();
+        /** @var HandlerStack $stack */
+        $stack = $config['stack'];
+        $stack->push(function($handler) {
+            return function (RequestInterface $request, array $options) use ($handler) {
+                $timeBeforeRequest = microtime(true);
+                /** @var PromiseInterface $promise */
+                $promise = $handler($request, $options);
+                $promise->then(function (ResponseInterface $response = null) use ($request, $timeBeforeRequest, $options) {
+                    $this->logger->log($request, $response, array_merge($options['loggingInfo'], [
+                        'timeBeforeRequest' => $timeBeforeRequest,
+                        'timeAfterRespond' => microtime(true),
+                    ]));
+
+                    return $response;
+                }, function(\Throwable $e = null) use ($request, $timeBeforeRequest, $options) {
+                    $response = null;
+                    if ($e instanceof RequestException) {
+                        $response = $e->getResponse();
+                    }
+                    $this->logger->log($request, $response, array_merge($options['loggingInfo'], [
+                        'timeBeforeRequest' => $timeBeforeRequest,
+                        'timeAfterRespond' => microtime(true),
+                        'exception' => $e,
+                    ]));
+
+                    throw $e;
+                });
+            };
+        }, 'http_request_logging');
+    }
 }
